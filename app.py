b11bb966ab2b3e94bf3b3d00cadc9a11b856c87f
aryan baakle <aryanbaakle@gmail.com>
2025-03-08 16:57:03 +0530

    added database

--- app.py
+++ app.py
@@ -1,44 +1,62 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from database import db
+from models import User
 app=Flask(__name__, template_folder='templates')
 
 
-todos=[{'task':'sample todo', 'done': False}]
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///my_database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # Disable modification tracking
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+#todos=[{'task':'sample todo', 'done': False}]
 
 
 
 @app.route('/')
 def index():
+    todos=User.query.all()
     return render_template('index.html', todos=todos)
 
 @app.route('/add', methods=['POST'])
 def add():
-    todo = request.form['todo']
-    todos.append({'task':todo, 'done': False})
+    task = request.form['todo']
+    new_task = User(task=task, done=False)
+    db.session.add(new_task)
+    db.session.commit()
     return redirect(url_for("index"))
 
-@app.route('/edit/<int:index>', methods=["GET", "POST"])
-def edit(index):
-    todo = todos[index]
+@app.route('/edit/<int:id>', methods=["GET", "POST"])
+def edit(id):
+    todo = User.query.get_or_404(id)
     if request.method == "POST":
-        todo['task'] = request.form['todo']
+        todo.task = request.form['todo']
+        db.session.commit()
         return redirect(url_for("index"))
     else:
         return render_template("edit.html", todo=todo, index=index)
 
-@app.route("/check/<int:index>")
-def check(index):
-    todos[index]['done'] = not todos[index]['done']
+@app.route("/check/<int:id>")
+def check(id):
+    todo = User.query.get_or_404(id)
+    todo.done = not todo.done
+    db.session.commit()
     return redirect(url_for('index'))
 
-@app.route('/delete/<int:index>')
-def delete(index):
-    del todos[index]
+@app.route('/delete/<int:id>')
+def delete(id):
+    todo = User.query.get_or_404(id)
+    db.session.delete(todo)
+    db.session.commit()
     return redirect(url_for('index'))
 
-@app.route('/preview/<int:index>')
-def preview(index):
-    todo=todos[index]
+@app.route('/preview/<int:id>')
+def preview(id):
+    todo=User.query.get_or_404(id)
     return render_template('preview.html', todo=todo)
 
 
