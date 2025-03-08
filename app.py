22fa4bc204be7450fac3c7f3bd95a034d8c99827
aryan baakle <aryanbaakle@gmail.com>
2025-03-08 18:27:19 +0530

    few changes

--- app.py
+++ app.py
@@ -61,5 +61,7 @@ def preview(id):
 
 
 
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -4,3 +4,4 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(200), nullable=False)
     done = db.Column(db.Boolean, default=False)
+   
